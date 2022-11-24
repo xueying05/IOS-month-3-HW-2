eb1ebf3783a4a2456e9546efe6d90a38cb4130bf
Nuraiym <1991958945@qq.com>
2022-11-24 12:28:34 +0600

    Initial Commit

---
+++ 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by NURAIYM KUDAIAR KYZY on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
